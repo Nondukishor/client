377652680f2fdc05a7587c660ef83872a4b6adf9
nipu.chakraborty <nipu.chakraborty@bongobd.com>
2021-06-25 13:00:24 +0600

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+require('dotenv').config()
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const handlebars = require('express-handlebars');
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs',
+    defaultLayout: 'planB',
+    partialsDir: __dirname + '/views/partials/'
+}));
+
+app.use(express.static('public'))
+
+
+
+app.get('/', (req, res) => {
+    res.render('home', {layout: 'index', listExists: true});
+});
+
+app.listen(port, () => console.log(`App listening to port ${port}`));
